511f9a2a8c2f5f5918c6dc7badf1d384fd356be5
Ellmaer <ellmaer2003@gmail.com>
2025-07-16 22:47:08 -0700

    updated app
    currently the app is in a sort of working condition. basic structure along with styling has been given

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -1,4 +1,16 @@
+// Import CSS Styling
 import '../node_modules/modern-normalize/modern-normalize.css'
-import './style.css'
+import './styles/style.css'
+
+
+
+const cells = document.querySelectorAll<HTMLDivElement>(".ttt-grid-cell");
+
+cells.forEach(cell => {
+    cell.addEventListener("click", () => {
+        cell.innerHTML = cell.innerHTML.trim() === "X" ? "O" : "X";
+    })
+})
+
 
 
