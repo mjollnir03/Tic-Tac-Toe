bf576bea4dd6a49eb54d60d833869019c685ef32
Ellmaer Ranjber <ellmaer2003@gmail.com>
2025-07-21 21:07:53 -0700

    updated main UI

--- tic-tac-toe/src/main.ts
+++ tic-tac-toe/src/main.ts
@@ -1,16 +1,11 @@
 // Import CSS Styling
-import '../node_modules/modern-normalize/modern-normalize.css'
-import './styles/style.css'
-
-
+import "../node_modules/modern-normalize/modern-normalize.css";
+import "./styles/style.css";
 
 const cells = document.querySelectorAll<HTMLDivElement>(".ttt-grid-cell");
 
-cells.forEach(cell => {
+cells.forEach((cell) => {
     cell.addEventListener("click", () => {
         cell.innerHTML = cell.innerHTML.trim() === "X" ? "O" : "X";
-    })
-})
-
-
-
+    });
+});
